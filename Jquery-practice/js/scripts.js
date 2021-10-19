c2e85883a60d807a15c99a8d6f0dda5069afabe5
Christinamawel <christinamawel@gmail.com>
2021-10-19 10:53:52 -0700

    add light and dark mode
    Co-authored-by: Karen Axon <kaxonwa@gmqil.com>

--- Jquery-practice/js/scripts.js
+++ Jquery-practice/js/scripts.js
@@ -3,11 +3,22 @@ $(document).ready(function() {
     $("#cat-info").show();
     $("#dog-info").hide();
   });
-});
 
-$(document).ready(function() {
+
   $("#dog-img").click(function() {
     $("#dog-info").show();
     $("#cat-info").hide();
+  });
+
+
+
+  $("#light-mode-btn").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-mode");
+  });
+
+  $("#dark-mode-btn").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-mode");
   });
 });
